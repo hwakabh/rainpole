49e97e437430aa692c8fea7d66990b42d2be5042
hwakabh <hrykwkbys1024@gmail.com>
2024-12-18 05:09:37 +0900

    test: added base test codes for main.go.

---
+++ main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestDefaultRoute(t *testing.T) {
+	expected := "hello, developers"
+	mock := httptest.NewServer(http.HandlerFunc(DefaultRoute))
+	defer mock.Close()
+
+	resp, err := http.Get(mock.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	txt, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read response body")
+		t.Error(err)
+	}
+	resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		t.Errorf("GET %s: expected status code is %d; got %d", mock.URL, 200, resp.StatusCode)
+	}
+	if string(txt) != expected {
+		t.Errorf("expected body [ %v ]; got [ %v ]", expected, string(txt))
+	}
+}
+
+func TestHealthCheckRoute(t *testing.T) {
+	expected := "ok"
+	mock := httptest.NewServer(http.HandlerFunc(HealthCheckRoute))
+	defer mock.Close()
+
+	resp, err := http.Get(mock.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	txt, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Failed to read response body")
+		t.Error(err)
+	}
+	resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		t.Errorf("GET %s: expected status code is %d; got %d", mock.URL, 200, resp.StatusCode)
+	}
+	if string(txt) != expected {
+		t.Errorf("expected body [ %v ]; got [ %v ]", expected, string(txt))
+	}
+}
